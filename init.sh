1a22ac6380641cf102f5d9e702138750afb07cdd
tkancf <tkancf@users.noreply.github.com>
2016-11-11 01:40:13 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,6 +4,8 @@ ln -sf ~/src/github.com/tkancf/dotfiles/zsh/zshenv ~/.zshenv
 
 ln -sf ~/src/github.com/tkancf/dotfiles/zsh/zshrc ~/.zshrc
 
+ln -sf ~/src/github.com/tkancf/dotfiles/vimrc ~/.vimrc
+
 if [ -e ~/.config/nvim ]; then
   ln -sf ~/src/github.com/tkancf/dotfiles/nvim ~/.config/nvim
 else
