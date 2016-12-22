fa2a5fac1014082b997b700769a95806303f2cf9
tkancf <tkancf@users.noreply.github.com>
2016-12-22 11:13:19 +0900

    raspi

--- init.sh
+++ init.sh
@@ -4,6 +4,10 @@ ln -sf ~/src/github.com/tkancf/dotfiles/zsh/zshenv ~/.zshenv
 
 ln -sf ~/src/github.com/tkancf/dotfiles/zsh/zshrc ~/.zshrc
 
+ln -sf ~/src/github.com/tkancf/dotfiles/vimrc ~/.vimrc
+
+ln -sf ~/src/github.com/tkancf/dotfiles/tmux.conf ~/.tmux.conf
+
 if [ -e ~/.config/nvim ]; then
   ln -sf ~/src/github.com/tkancf/dotfiles/nvim ~/.config/nvim
 else
