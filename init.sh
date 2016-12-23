985e8637585c01d0dc053e5bb860768d9c480dcc
tkancf <tkancf@users.noreply.github.com>
2016-12-23 20:51:53 +0900

    zsh backup

--- init.sh
+++ init.sh
@@ -6,6 +6,8 @@ ln -sf ~/src/github.com/tkancf/dotfiles/zsh/zshrc ~/.zshrc
 
 ln -sf ~/src/github.com/tkancf/dotfiles/vimrc ~/.vimrc
 
+ln -sf ~/src/github.com/tkancf/dotfiles/vim/ctrlp-launcher ~/.ctrlp-launcher
+
 ln -sf ~/src/github.com/tkancf/dotfiles/tmux.conf ~/.tmux.conf
 
 if [ -e ~/.config/nvim ]; then
