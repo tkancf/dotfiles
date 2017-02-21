63db1eb326f46502b7f50d8d8567dd3fbddf0abe
tkancf <tkancf@users.noreply.github.com>
2017-02-21 19:25:59 +0900

    Remove unnecessary lines

--- init.sh
+++ init.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
-#ln -sf ~/src/github.com/tkancf/dotfiles/zsh/zshenv ~/.zshenv
-
-#ln -sf ~/src/github.com/tkancf/dotfiles/zsh/zshrc ~/.zshrc
-
-ln -sf ~/src/github.com/tkancf/dotfiles/zprezto ~/.zprezto
-
 ln -sf ~/src/github.com/tkancf/dotfiles/vimrc ~/.vimrc
 
 ln -sf ~/src/github.com/tkancf/dotfiles/vim/ctrlp-launcher ~/.ctrlp-launcher
 
 ln -sf ~/src/github.com/tkancf/dotfiles/tmux.conf ~/.tmux.conf
-
-if [ -e ~/.config/nvim ]; then
-  ln -sf ~/src/github.com/tkancf/dotfiles/nvim ~/.config/nvim
-else
-  mkdir -p ~/.config
-  ln -sf ~/src/github.com/tkancf/dotfiles/nvim ~/.config/nvim
-fi
