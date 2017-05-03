777901ad6dae5f8b0f24e3d2605e1b46d0f93e86
tkancf <tkancf@users.noreply.github.com>
2017-05-03 23:40:54 +0900

    Fix initialize file

--- init.sh
+++ init.sh
@@ -2,6 +2,8 @@
 
 ln -sf ~/src/github.com/tkancf/dotfiles/vimrc ~/.vimrc
 
+ln -sf ~/src/github.com/tkancf/dotfiles/vim/ ~/.vim
+
 ln -sf ~/src/github.com/tkancf/dotfiles/vim/ctrlp-launcher ~/.ctrlp-launcher
 
 ln -sf ~/src/github.com/tkancf/dotfiles/tmux.conf ~/.tmux.conf
