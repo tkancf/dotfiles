0a89c4b3691d0b8e2631c52e62ca231c3f4bce7d
tkancf <tkancf@users.noreply.github.com>
2017-11-29 15:28:02 +0900

    Delete unused files

--- vim-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git pull
-./configure --with-features=huge --enable-gui=gnome2 \
-  --enable-perlinterp --enable-pythoninterp \
-  --enable-python3interp --enable-rubyinterp \
-  --enable-luainterp --with-luajit \
-  --enable-fail-if-missing \
-make
