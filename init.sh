a0e7a2a7c805e9e5c47caa3c224ccf58daeca9ad
tkancf <tkncf789@gmail.com>
2019-04-30 07:55:51 +0900

    update

--- init.sh
+++ init.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+# vimrc
 ln -sf ~/.dotfiles/vimrc ~/.vimrc
-
 ln -sf ~/.dotfiles/vim/ ~/.vim/
-
 ln -sf ~/.dotfiles/vim/ctrlp-launcher ~/.ctrlp-launcher
 
+# bashrc
 ln -sf ~/.dotfiles/bashrc ~/.bashrc
 
+# tmux
 ln -sf ~/.dotfiles/tmux.conf ~/.tmux.conf
 
+# gitconfig
 ln -sf ~/.dotfiles/gitconfig ~/.gitconfig
 
+# zsh
 ln -sf ~/.dotfiles/zshrc ~/.zshrc
-
 ln -sf ~/.dotfiles/zprofile ~/.zprofile
