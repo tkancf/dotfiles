608e906f97efdfbf7eeb25d433aa0d836ea5d4e6
tkancf <tkncf789@gmail.com>
2019-04-30 09:39:16 +0900

    update

--- init.sh
+++ init.sh
@@ -3,9 +3,9 @@
 # vimrc
 ln -sf ~/.dotfiles/vimrc ~/.vimrc
 ln -sf ~/.dotfiles/vim/ctrlp-launcher ~/.ctrlp-launcher
-ln -sf ~/.dotfiles/snippets ~/.vim
-ln -sf ~/.dotfiles/template ~/.vim
-ln -sf ~/.dotfiles/tmp ~/.vim
+ln -sf ~/.dotfiles/snippets/ ~/.vim/
+ln -sf ~/.dotfiles/template/ ~/.vim/
+ln -sf ~/.dotfiles/tmp/ ~/.vim/
 
 # bashrc
 ln -sf ~/.dotfiles/bashrc ~/.bashrc
