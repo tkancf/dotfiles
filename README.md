1aa8361260b1e3d49e8a30f3043e6a9fe69bf14f
tkancf <tkncf789@gmail.com>
2023-02-09 22:54:49 +0900

    Delete all
    Delete all files

--- README.md
+++
@@ -1,7 +0,0 @@
-# dotfiles
-
-## install commands
-
-```bash
-git clone --depth 1 https://github.com/tkancf/dotfiles $HOME/.dotfiles;cd $HOME/.dotfiles;make all
-```
--- scripts/setup.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -eux
-# shellcheck source=./scripts/common.bash
-source "$(dirname "$0")/common.bash"
-
-/bin/bash "$CUR_DIR/setup-links.bash"
-/bin/bash "$CUR_DIR/setup-asdf.bash"
-/bin/bash "$CUR_DIR/setup-sheldon.bash"
