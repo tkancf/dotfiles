1f7b20f95a1b1809ff3e12a9ed4d04204541e223
tkancf <18424095+tkancf@users.noreply.github.com>
2025-01-13 09:18:32 +0900

    nvim: update plugin lockfile and refactor completion plugins

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,35 +1,53 @@
-return {}
---[[
 return {
   {
     'saghen/blink.cmp',
-    version = '0.*',
+    version = false,
     dependencies = {
-      -- add source
-      { "saghen/blink.compat", lazy = true, version = false }, },
-    sources = {
-      -- remember to enable your providers here
-      default = { 'lsp', 'path', 'snippets', 'buffer', 'digraphs' },
-      compat = { "obsidian", "obsidian_new", "obsidian_tags" },
-      providers = {
-        -- create provider
-        digraphs = {
-          name = 'digraphs', -- IMPORTANT: use the same name as you would for nvim-cmp
-          module = 'blink.compat.source',
+      { 'rafamadriz/friendly-snippets' },
+      { "saghen/blink.compat", lazy = true, version = false },
+      { "https://github.com/epwalsh/obsidian.nvim" },
+    },
 
-          -- all blink.cmp source config options work as normal:
-          score_offset = -3,
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      -- 'default' for mappings similar to built-in completion
+      -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+      -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+      -- See the full "keymap" documentation for information on defining your own keymap.
+      keymap = { preset = 'enter' },
+      completion = { list = { selection = { preselect = false, auto_insert = false } } },
+      appearance = {
+        -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+        -- Useful for when your theme doesn't support blink.cmp
+        -- Will be removed in a future release
+        use_nvim_cmp_as_default = true,
+        -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = 'mono'
+      },
 
-          -- this table is passed directly to the proxied completion source
-          -- as the `option` field in nvim-cmp's source config
-          --
-          -- this is NOT the same as the opts in a plugin's lazy.nvim spec
-          opts = {
-            -- this is an option from cmp-digraphs
-            cache_digraphs_on_start = true,
+      -- Default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, due to `opts_extend`
+      sources = {
+        default = { "obsidian", "obsidian_new", "obsidian_tags", "lsp", "path", "snippets", "buffer" },
+        providers = {
+          obsidian = {
+            name = "obsidian",
+            module = "blink.compat.source",
+          },
+          obsidian_new = {
+            name = "obsidian_new",
+            module = "blink.compat.source",
+          },
+          obsidian_tags = {
+            name = "obsidian_tags",
+            module = "blink.compat.source",
           },
         },
       },
     },
-  },
-}]]
+    opts_extend = { "sources.default" }
+  }
+}
+
