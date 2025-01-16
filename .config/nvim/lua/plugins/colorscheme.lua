2cffab7d2f71ad4a120b6ff6af0f6595440295c3
tkancf <18424095+tkancf@users.noreply.github.com>
2025-01-16 15:49:42 +0900

    Add lazy setting to color scheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,7 @@
 return {
     {
         'https://github.com/navarasu/onedark.nvim',
-        lazy = false,    -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
+        event = 'VimEnter',
         config = function()
             require('onedark').setup {
                 style = 'warm'
