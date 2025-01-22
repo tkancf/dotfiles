899adfb63ea9ad591acc2e1a930a0a21e12fcf4a
tkancf <18424095+tkancf@users.noreply.github.com>
2025-01-22 18:59:49 +0900

    Update nvim

---
+++ .config/nvim/lua/plugins/oil.nvim.lua
@@ -0,0 +1,8 @@
+return {
+  'https://github.com/stevearc/oil.nvim',
+  event = "BufRead",
+  config = function()
+    require("oil").setup()
+    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+  end
+}
