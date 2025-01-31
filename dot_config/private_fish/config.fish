009a15cf3a061c06fc48130b4ff3a2d1f8653200
tkancf <18424095+tkancf@users.noreply.github.com>
2025-01-31 10:53:33 +0900

    Add fzf.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,7 +1,14 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+  # Homebrew
+  eval (/opt/homebrew/bin/brew shellenv)
+
+  # mise
+  ## do nothing! mise is automatically activated when using brew and fish
+  ## you can disable this behavior with `set -Ux MISE_FISH_AUTO_ACTIVATE 0`
 end
 # fish起動時のメッセージ削除
 set fish_greeting
 
-abbr -a cnv "cd ~/.config/nvim/ && nvim ./lua/plugins/"
+# abbr
+abbr -a cnn "cd ~/.config/nvim/ && nvim ./lua/plugins/ # edit neovim config"
+abbr -a cnf "cd ~/.config/fish/ && nvim config.fish # edit fish config"
