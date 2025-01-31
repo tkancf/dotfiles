b46351972dcc2419e298398a168b29a40ca9551b
tkancf <18424095+tkancf@users.noreply.github.com>
2025-01-31 13:12:22 +0900

    Add zoxide config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -5,16 +5,21 @@ if status is-interactive
   # mise
   ## do nothing! mise is automatically activated when using brew and fish
   ## you can disable this behavior with `set -Ux MISE_FISH_AUTO_ACTIVATE 0`
+
+
+  # zoxide
+  zoxide init fish | source
 end
 # fish起動時のメッセージ削除
 set fish_greeting
+set -x XDG_CONFIG_HOME "$HOME/.config"
 
 # abbr
 ## edit config
 abbr -a cnn "cd ~/.config/nvim/ && nvim ./lua/plugins/ # edit neovim config"
 abbr -a cnf "cd ~/.config/fish/ && nvim config.fish # edit fish config"
-abbr -a czc "chezmoi cd"
-abbr -a cza "chezmoi re-add ~/.config/"
+abbr -a cc "chezmoi cd"
+abbr -a ca "chezmoi re-add ~/.config/"
 abbr -a cz "chezmoi"
 
 ## git
@@ -24,6 +29,8 @@ abbr -a ga. "git add ."
 abbr -a gcm "git commit -m"
 abbr -a gd "git diff"
 abbr -a gdc "git diff --cached"
+abbr -a gg "git log --graph --date-order --all --pretty=format:'%h %Cred%d %Cgreen%ad %Cblue%cn %Creset%s' --date=short"
 
 ## fzf
 abbr -a bb "abbr --show | fzf"
+abbr -a cdg "cd (ghq list --full-path | fzf)"
