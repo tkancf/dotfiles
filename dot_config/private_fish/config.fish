3e8aa91db99968eef3d7ddd2ee45819e26044674
tkancf <18424095+tkancf@users.noreply.github.com>
2025-01-31 11:00:17 +0900

    Add fish abbr

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -10,5 +10,20 @@ end
 set fish_greeting
 
 # abbr
+## edit config
 abbr -a cnn "cd ~/.config/nvim/ && nvim ./lua/plugins/ # edit neovim config"
 abbr -a cnf "cd ~/.config/fish/ && nvim config.fish # edit fish config"
+abbr -a czc "chezmoi cd"
+abbr -a cza "chezmoi re-add ~/.config/"
+abbr -a cz "chezmoi"
+
+## git
+abbr -a gs "git status"
+abbr -a ga "git add"
+abbr -a ga. "git add ."
+abbr -a gcm "git commit -m"
+abbr -a gd "git diff"
+abbr -a gdc "git diff --cached"
+
+## fzf
+abbr -a bb "abbr --show | fzf"
