8b92d7b016cd05e1425aabbecdc6a94b6bf9bca5
tkancf <18424095+tkancf@users.noreply.github.com>
2025-02-28 13:24:06 +0900

    Update options

--- dot_config/nvim/lua/plugins/avante.nvim.lua
+++ dot_config/nvim/lua/plugins/avante.nvim.lua
@@ -4,16 +4,46 @@ return {
   lazy = false,
   version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
   opts = {
-    -- add any opts here
-    -- for example
-    provider = "openai",
-    openai = {
-      endpoint = "https://api.openai.com/v1",
-      model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-      timeout = 30000, -- timeout in milliseconds
-      temperature = 0, -- adjust if needed
+    provider = "copilot",
+    -- provider = "claude",
+    -- provider = "openai",
+    auto_suggestions_provider = "copilot",
+    behaviour = {
+      auto_suggestions = true,
+      auto_set_highlight_group = true,
+      auto_set_keymaps = true,
+      auto_apply_diff_after_generation = true,
+      support_paste_from_clipboard = true,
+    },
+    windows = {
+      position = "right",
+      width = 30,
+      sidebar_header = {
+        align = "center",
+        rounded = false,
+      },
+      ask = {
+        floating = true,
+        start_insert = true,
+        border = "rounded"
+      }
+    },
+    -- providers-setting
+    claude = {
+      model = "claude-3-5-sonnet-20240620", -- $3/$15, maxtokens=8000
+      -- model = "claude-3-opus-20240229",  -- $15/$75
+      -- model = "claude-3-haiku-20240307", -- $0.25/1.25
+      max_tokens = 8000,
+    },
+    copilot = {
+      model = "gpt-4o-2024-05-13",
+      -- model = "gpt-4o-mini",
       max_tokens = 4096,
-      -- reasoning_effort = "high" -- only supported for reasoning models (o1, etc.)
+    },
+    openai = {
+      model = "gpt-4o", -- $2.5/$10
+      -- model = "gpt-4o-mini", -- $0.15/$0.60
+      max_tokens = 4096,
     },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
