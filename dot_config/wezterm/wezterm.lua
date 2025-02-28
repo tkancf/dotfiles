1202f47bfd62f936fc8096fc56b76f2ead393414
tkancf <18424095+tkancf@users.noreply.github.com>
2025-02-28 12:39:51 +0900

    Add keybind

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -5,18 +5,64 @@ config.color_scheme = 'Molokai'
 config.window_background_opacity = 0.85
 config.hide_tab_bar_if_only_one_tab = true
 config.font = wezterm.font_with_fallback({
-  { family = "Hack Nerd Font Mono" },
-  { family = "Hack Nerd Font Mono", assume_emoji_presentation = true },
+  { family = "Cica" },
+  { family = "Cica", assume_emoji_presentation = true },
 })
 
 config.keys = {
-  { key="t", mods="CMD", action=wezterm.action{SpawnTab="CurrentPaneDomain"}},
-  { key="w", mods="CMD", action=wezterm.action{CloseCurrentTab={confirm=true}}},
-  { key = '5', mods = 'CMD', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }},
-  { key = '\'', mods = 'CMD', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }},
-  { key="c", mods="CMD", action=wezterm.action{CopyTo="Clipboard"}},
-  { key="v", mods="CMD", action=wezterm.action{PasteFrom="Clipboard"}},
-  { key="Insert", mods="CMD", action=wezterm.action{PasteFrom="Clipboard"}},
+  -- タブ操作
+  {
+    key = "t",
+    mods = "CMD",
+    action = wezterm.action{ SpawnTab = "CurrentPaneDomain" }
+  },
+  {
+    key = "w",
+    mods = "CMD",
+    action = wezterm.action{ CloseCurrentTab = { confirm = true } }
+  },
+  {
+    key = "n",
+    mods = "CMD",
+    action = wezterm.action.ActivateTabRelative(1)
+  },
+  {
+    key = "p",
+    mods = "CMD",
+    action = wezterm.action.ActivateTabRelative(-1)
+  },
+  {
+    key = "e",
+    mods = "CMD",
+    action = wezterm.action.ShowTabNavigator
+  },
+  -- ペイン操作
+  {
+    key = "5",
+    mods = "CMD",
+    action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" }
+  },
+  {
+    key = "'",
+    mods = "CMD",
+    action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" }
+  },
+  {
+    key = "z",
+    mods = "CMD",
+    action = wezterm.action.TogglePaneZoomState
+  },
+  -- クリップボード操作
+  {
+    key = "c",
+    mods = "CMD",
+    action = wezterm.action{ CopyTo = "Clipboard" }
+  },
+  {
+    key = "v",
+    mods = "CMD",
+    action = wezterm.action{ PasteFrom = "Clipboard" }
+  },
 }
 
 return config
