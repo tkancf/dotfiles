8dfbdb61680f67aa824cb10fa44577de94bfb9ec
tkancf <18424095+tkancf@users.noreply.github.com>
2025-03-12 22:36:55 +0900

    Update neovim config

--- dot_config/nvim/lua/plugins/codecompanion.lua
+++ dot_config/nvim/lua/plugins/codecompanion.lua
@@ -1,29 +1,36 @@
 return {
-  {
-    "olimorris/codecompanion.nvim",
-    event = 'WinEnter',
-    opts = {
-      language = "日本語",
-      adapters = {
-        ollama = function()
-          return require("codecompanion.adapters").extend("ollama", {
-            schema = {
-              model = {
-                default = "codellama:7b",
-              },
+  "olimorris/codecompanion.nvim",
+  event = 'WinEnter',
+  opts = {
+    language = "日本語",
+    adapters = {
+      ollama = function()
+        return require("codecompanion.adapters").extend("ollama", {
+          schema = {
+            model = {
+              default = "codellama:7b",
             },
-          })
-        end,
+          },
+        })
+      end,
+      claude_3_7_sonnet = function()
+        return require("codecompanion.adapters").extend("ollama", {
+          schema = {
+            model = {
+              default = "codellama:7b",
+            },
+          },
+        })
+      end,
+    },
+    strategies = {
+      -- Change the default chat adapter
+      chat = {
+        adapter = "copilot",
       },
-      strategies = {
-        -- Change the default chat adapter
-        chat = {
-          adapter = "copilot",
-        },
-        inline = {
-          adapter = "copilot",
-        },
+      inline = {
+        adapter = "copilot",
       },
     },
-  }
+  },
 }
