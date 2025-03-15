fa05f0963c74002edc27496f1c361e7c4844c0f2
tkancf <18424095+tkancf@users.noreply.github.com>
2025-03-15 10:23:09 +0900

    Update key config

--- dot_config/karabiner-config/src/index.ts
+++ dot_config/karabiner-config/src/index.ts
@@ -6,29 +6,29 @@ import {
 
 // karabiner-elementsの設定をkarabiner.tsで作成
 writeToProfile('karabiner-ts', [
-  // バッククオートとチルダを割り当てる
   rule('バッククオートとチルダを割り当てる。').manipulators([
     map('international3', 'optionalAny').to('grave_accent_and_tilde'),
   ]),
 
-  // Shiftキーをオプションキーに置き換える
   rule('Shiftキーをオプションキーに置き換える').manipulators([
-    map('left_shift').to('left_option'),
-    map('right_shift').to('right_option'),
+    map('left_shift', 'optionalAny').to('left_option'),
+    map('right_shift', 'optionalAny').to('right_option'),
   ]),
 
-  // スペースキーを単体で押したときはスペース、他のキーと組み合わせたときは左コントロール
+  rule('CommandキーをShiftに置き換える').manipulators([
+    map('left_command', 'optionalAny').to('left_shift'),
+    map('right_command', 'optionalAny').to('right_shift'),
+  ]),
+
+  rule('英数・かなキーを他のキーと組み合わせて押したときに、Commandキーを送信する。').manipulators([
+    map('japanese_eisuu').to('left_command').toIfAlone('japanese_eisuu'),
+    map('japanese_kana').to('right_command').toIfAlone('japanese_kana'),
+  ]),
+
   rule('スペースキーを単体で押したときはスペース、他のキーと組み合わせたときは左コントロール').manipulators([
     map('spacebar').to('left_control').toIfAlone('spacebar'),
   ]),
 
-  // 英数・かなキーを他のキーと組み合わせて押したときに、Shiftキーを送信する
-  rule('英数・かなキーを他のキーと組み合わせて押したときに、Shiftキーを送信する。').manipulators([
-    map('japanese_eisuu').to('left_shift').toIfAlone('japanese_eisuu'),
-    map('japanese_kana').to('right_shift').toIfAlone('japanese_kana'),
-  ]),
-
-  // caps lockをhyper keyに割り当てる
   rule('Caps Lock → Hyper').manipulators([
     map('caps_lock').toHyper(),
   ]),
