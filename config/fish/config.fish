c3b6680a43bf11cf1b9ee9730104cc25db9ae5d7
tkancf <18424095+tkancf@users.noreply.github.com>
2025-11-02 16:54:17 +0900

    Update fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -60,3 +60,7 @@ fish_add_path "/Users/tkan/.local/bin/zig-aarch64-macos-0.15.1/"
 source $HOME/.config/local.fish
 
 zoxide init fish | source
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
