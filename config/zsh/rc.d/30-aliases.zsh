0960e89eb8f60f6cdccf5e9a4bed51320efeae0d
tkancf <18424095+tkancf@users.noreply.github.com>
2025-11-03 22:29:34 +0900

    Split zshrc

---
+++ config/zsh/rc.d/30-aliases.zsh
@@ -0,0 +1,25 @@
+# Common aliases for shell tools.
+
+# basics
+alias ll='ls -alF'
+alias l='ls -CF'
+alias la='ls -aF'
+alias t='tmux -2'
+alias vi='vim'
+
+# git
+alias g='git'
+alias gl='git log --graph'
+alias gg='git graph'
+alias ga='git add .'
+alias gs='git status'
+alias gd='git diff'
+alias gdc='git diff --cached'
+alias gc='git commit'
+alias gm='git commit -m'
+
+# ssh
+alias s='ssh'
+
+# nvim
+alias n='nvim'
