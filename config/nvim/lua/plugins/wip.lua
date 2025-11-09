cd4388bd86187061ab6fa61a9c8d43f86930deb5
tkancf <18424095+tkancf@users.noreply.github.com>
2025-11-09 22:33:33 +0900

    Update

--- config/nvim/lua/plugins/wip.lua
+++ config/nvim/lua/plugins/wip.lua
@@ -1,23 +1,23 @@
 return {
-    {
-        'stevearc/oil.nvim',
-        ---@module 'oil'
-        ---@type oil.SetupOpts
-        opts = {},
-        dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-        lazy = false,
-    },
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "MunifTanjim/nui.nvim",
-            "nvim-tree/nvim-web-devicons",
-        },
-        lazy = false, -- neo-tree will lazily load itself
-        config = function()
-            vim.keymap.set('n', '<leader>e', '<CMD>:Neotree<CR>', { desc = 'Open file explorer' })
-        end,
-    },
+	{
+		'stevearc/oil.nvim',
+		---@module 'oil'
+		---@type oil.SetupOpts
+		opts = {},
+		dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+		lazy = false,
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			-- "nvim-tree/nvim-web-devicons",
+		},
+		lazy = false, -- neo-tree will lazily load itself
+		config = function()
+			vim.keymap.set('n', '<leader>e', '<C-u>:Neotree<CR>', { desc = 'Open file explorer' })
+		end,
+	},
 }
