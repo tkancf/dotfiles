bc1a2f71a776f17b515f22ef4a409165b26dfb7d
tkancf <18424095+tkancf@users.noreply.github.com>
2025-11-29 22:48:42 +0900

    Update lua_lsp config

--- config/nvim/after/lsp/lua_ls.lua
+++ config/nvim/after/lsp/lua_ls.lua
@@ -2,9 +2,15 @@
 return {
     settings = {
         Lua = {
+            diagnostics = {
+                globals = { "vim" },
+            },
             workspace = {
+                -- Neovim の runtime と lazy.nvim の plugin を全て読む
                 library = {
-                    vim.env.VIMRUNTIME .. "/lua",
+                    vim.env.VIMRUNTIME,
+                    vim.fn.expand("~/.local/share/nvim/lazy"),
+                    vim.fn.stdpath("config") .. "/lua",
                 },
             },
         },
