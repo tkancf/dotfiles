8a94cda40564584f4874f93c10fede62a1d2cde9
tkan <18424095+tkancf@users.noreply.github.com>
2025-12-09 12:31:41 +0900

    Fix source path for local fish configuration

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -57,7 +57,7 @@ fish_add_path "$HOME/.local/bin"
 fish_add_path "/Users/tkan/.local/bin/roc_nightly-macos_apple_silicon-2025-09-09-d73ea109cc2/"
 fish_add_path "/Users/tkan/.local/bin/zig-aarch64-macos-0.15.1/"
 
-source $HOME/.config/local.fish
+source $HOME/.config/fish/local.fish
 
 zoxide init fish | source
 
