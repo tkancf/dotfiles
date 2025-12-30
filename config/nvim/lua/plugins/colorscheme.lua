7110181e5d33aa8cd8246c366e68d45a8cc8b820
tkancf <18424095+tkancf@users.noreply.github.com>
2025-12-31 01:10:54 +0900

    colorschmeをTokyoNight Moonへ変更

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,9 @@
--- catppuccin のカラースキームを適用する設定
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme "catppuccin-frappe"
-  end
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+        vim.cmd("colorscheme tokyonight-moon")
+    end,
 }
