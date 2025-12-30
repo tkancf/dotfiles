90874ebe4033ef0747e8d7dbe2b49b824140a347
tkancf <18424095+tkancf@users.noreply.github.com>
2025-12-31 01:43:57 +0900

    quicker.nvimを追加

--- config/nvim/lua/plugins/quicker.lua
+++ config/nvim/lua/plugins/quicker.lua
@@ -1,6 +1,41 @@
 -- Quickfix/location list UI enhancements
 return {
     "stevearc/quicker.nvim",
-    event = "VeryLazy",
-    opts = {},
+    ft = "qf",
+    keys = {
+        {
+            "<leader>q",
+            function() require("quicker").toggle() end,
+            desc = "Toggle quickfix",
+        }
+    },
+    opts = {
+        edit = {
+            enabled = true,
+            autosave = "unmodified",
+        },
+        constrain_cursor = true,
+        highlight = {
+            treesitter = true,
+            lsp = true,
+            load_buffers = false,
+        },
+        keys = {
+            {
+                ">",
+                function() require("quicker").expand({ before = 2, after = 2 }) end,
+                desc = "Expand quickfix context",
+            },
+            {
+                "<",
+                function() require("quicker").collapse() end,
+                desc = "Collapse quickfix context",
+            },
+            {
+                "R",
+                function() require("quicker").refresh() end,
+                desc = "Refresh quickfix",
+            },
+        },
+    },
 }
