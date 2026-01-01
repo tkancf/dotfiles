bdb668c8916c9c93c6120403c7fca7c363c72fca
tkancf <18424095+tkancf@users.noreply.github.com>
2026-01-02 00:38:15 +0900

    カラースキーム設定更新

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,9 @@
+-- カラースキーム設定
 return {
-    "folke/tokyonight.nvim",
-    lazy = false,
+    "catppuccin/nvim",
+    name = "catppuccin",
     priority = 1000,
-    opts = {},
     config = function()
-        vim.cmd("colorscheme tokyonight-moon")
-    end,
+        vim.cmd.colorscheme "catppuccin-frappe"
+    end
 }
