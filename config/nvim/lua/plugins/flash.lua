35b8426d2bac577fa35bf7d3282142cf411944e3
tkancf <18424095+tkancf@users.noreply.github.com>
2026-01-03 14:32:49 +0900

    flash.nvimの起動をssのみに変更

--- config/nvim/lua/plugins/flash.lua
+++ config/nvim/lua/plugins/flash.lua
@@ -2,7 +2,13 @@
 return {
     "folke/flash.nvim",
     event = "VeryLazy",
-    opts = {},
+    opts = {
+        modes = {
+            char = {
+                enabled = false,
+            },
+        },
+    },
     keys = {
         { "ss", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     },
